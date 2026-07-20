78b91cfe7d1ff54d182326ee71144ceeaceadf76
ayush2726-maker <ayush2726@gmail.com>
2026-07-20 16:59:44 +0530

    Enable auth recovery enhancements before app load

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
+import { registerRootComponent } from "expo";
+
+const { installAuthEnhancements } = require("./src/runtime/AuthEnhancements");
+installAuthEnhancements();
+
+const AppModule = require("./App");
+const App = AppModule.default || AppModule;
 
 registerRootComponent(App);
