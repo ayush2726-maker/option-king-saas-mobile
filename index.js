3c33d0545915252aaa44891f7a137777012931a6
ayush2726-maker <ayush2726@gmail.com>
2026-07-31 15:40:46 +0530

    Install multi-open trade UI before app wrappers load

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import './src/polyfills';
 import { registerRootComponent } from 'expo';
-import App from './AppTradeExplanationPatched';
+
+// Install before AppPatched loads LiveScoreTradeTabEnhancement. This lets the
+// multi-trade wrapper replace the single Active Trade card and suppress the old
+// global floating exit button.
+const { installMultiOpenTradeEnhancement } = require(
+  './src/runtime/MultiOpenTradeEnhancement'
+);
+installMultiOpenTradeEnhancement();
+
+const AppModule = require('./AppTradeExplanationPatched');
+const App = AppModule.default || AppModule;
 
 registerRootComponent(App);
